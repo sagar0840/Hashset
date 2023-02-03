f40ef2524da1bf85763f7b7d309b59227d876b34
sagar0840 <sagarkale55556@gmail.com>
2023-02-03 16:55:50 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set1 = new HashSet<string>();
+
+            set1.Add("C");
+            set1.Add("C++");
+            set1.Add("C#");
+            set1.Add("Java");
+            set1.Add("Ruby");
+
+            HashSet<string> set2 = new HashSet<string>();
+
+            set2.Add("PHP");
+            set2.Add("C++");
+            set2.Add("Perl");
+            set2.Add("Java");
+
+          
+
+             set1.UnionWith(set2);
+             // set1.IntersectWith(set2);
+           // set1.ExceptWith(set2);
+            foreach (string s in set1)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+
+    }
+}
+
